c14f860991bdec5d9212bdd879553e1d611f36b5
jindo takumi <92016292+jtakumi@users.noreply.github.com>
2022-05-19 20:26:05 +0900

    indipendent ver created

---
+++ youtube_ind_git.py
@@ -0,0 +1,80 @@
+from googleapiclient.discovery import build
+import json
+import os
+import datetime
+import sys
+
+
+"""
+参考サイト
+https://qiita.com/rkamikawa/items/dd1fd4c1427ece787eea
+https://ccie-go.com/python-json/#toc9
+https://techacademy.jp/magazine/23279
+"""
+
+def read_json():
+    key=input()
+    fnp='search/search_' + key + '.json'
+
+    with open(fnp,"r",encoding='utf-8') as f:
+        jsn=json.load(f)
+        #idというkeyの中のchannelIdを取り出す
+        return jsn['id']['channelId']
+
+def file_make(fn,search_respose):
+    with open(fn,'w',encoding='utf-8') as f:
+        for search_result in search_respose.get('items',[]):
+            if search_result['kind'] !='youtube#channel':
+                continue
+            print(json.dumps(search_result,indent=2,ensure_ascii=False),file=f)
+
+    #できたファイルを移動させる
+    mvf='move'+ ' '+ fn + ' ' + 'youtube-json'
+    os.system(mvf)
+
+def git(fn):
+    #自動commit
+    gad='git add ' + 'youtube-json/'+ fn
+    os.system(gad)
+    print('Do you want to commit? [y/n]')
+    dc=input()
+    if(dc=='y'):
+        today=datetime.date.today()
+        d1=today.strftime('%y-%m-%d')
+        gcm='git commit -m ' + d1
+        os.system(gcm)
+    else:
+        print('Bye')
+
+
+
+#API情報
+API_KEY='own_key'
+YOUTUBE_API_SERVICE_NAME='youtube'
+YOUTUBE_API_VERSION='v3'
+
+print("please input the keyword.")
+search_word=read_json()
+
+
+#jsonファイルに結果を書きこむ
+print('please input file name.(no extension)')
+fn=input()
+fn=fn+'.json'
+
+
+youtube=build(
+    YOUTUBE_API_SERVICE_NAME,
+    YOUTUBE_API_VERSION,
+    developerKey = API_KEY
+)
+
+search_respose=youtube.channels().list(
+    part='snippet,statistics',
+    id=search_word
+).execute()
+
+file_make(fn,search_respose)
+git(fn)
+
+
