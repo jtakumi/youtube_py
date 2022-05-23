c3f42554321b4e220a39e7905f1327a798b8f238
jindo takumi <92016292+jtakumi@users.noreply.github.com>
2022-05-23 17:44:38 +0900

    22-05-23

--- count_youtube.py
+++ count_youtube.py
@@ -9,14 +9,15 @@ today=today.strftime('%y/%m/%d')
 with open('hololive_members.csv','w',encoding='utf-8') as f:
     print('---hololive members---',file=f)
     print('date:',today,file=f)
+    print(file=f)
     for i in os.listdir(dir):
         c+=1
         #ファイルのパスを取得
         path=os.path.join(dir,i)
         #拡張子なしでファイル名を取得
         member=os.path.splitext( os.path.basename(path))[0]
-        print(member)
         print(member,file=f)
+    print(file=f)
     print("member count=",c,file=f)
 
 os.system('git add count_youtube.py hololive_members.csv')
