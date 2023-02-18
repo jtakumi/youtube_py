88aa85d9d82e9e1d5311f0a3b2156350b35ddf69
jindo takumi <92016292+jtakumi@users.noreply.github.com>
2023-02-18 21:42:48 +0900

    class convert

---
+++ looptest.py
@@ -0,0 +1,9 @@
+import os
+from os import listdir
+
+dir='./hololive'
+
+i=1
+for i in range(1,len(listdir(dir))):
+    print(i)
+    
--- search_youtube_git.py
+++ search_youtube_git.py
@@ -8,58 +8,59 @@ import datetime
 https://qiita.com/rkamikawa/items/dd1fd4c1427ece787eea
 """
 
+
+
 #API情報
-API_KEY='own_key'
+API_KEY='key'
 YOUTUBE_API_SERVICE_NAME='youtube'
 YOUTUBE_API_VERSION='v3'
-print('please input a  keyword.')
-search_word=input()
+class Search_Youtube:
+    def make_file(self,fn,search_word):
 
-#jsonファイルに結果を書きこむ
+        youtube=build(
+            YOUTUBE_API_SERVICE_NAME,
+            YOUTUBE_API_VERSION,
+            developerKey = API_KEY
+        )
 
-fn='search/search_' + search_word + '.json'
+        search_respose=youtube.search().list(
+            q=search_word,
+            part='id,snippet',
+            maxResults=25
+        ).execute()
 
-
-youtube=build(
-    YOUTUBE_API_SERVICE_NAME,
-    YOUTUBE_API_VERSION,
-    developerKey = API_KEY
-)
-
-search_respose=youtube.search().list(
-    q=search_word,
-    part='id,snippet',
-    maxResults=25
-).execute()
-
-
-def make_file(fn):
-    with open(fn,'w',encoding='utf-8') as f:
-        wf=0
-        #検索結果が複数なら","をエラー対策でつけたい
-        for search_result in search_respose.get('items',[]):
-            if wf==0:
-                if search_result['id']['kind'] !='youtube#channel':
-                    continue
-                print(json.dumps(search_result,indent=2,ensure_ascii=False),file=f)
-                wf=1
-            else:
-                break
-
-def git():
-    #自動commit
-    gad='git add ' + fn
-    os.system(gad)
-    print('Do you want to move to youtube.py?[y/n]')
-    dc=input()
-    if(dc=='y'):
-        import youtube
-        youtube.main()
-    else:
-            print('Bye')
-
-def main():
-    make_file()
-    git()
+        with open(fn,'w',encoding='utf-8') as f:
+            wf=0
+            #検索結果が複数なら","をエラー対策でつけたい
+            for search_result in search_respose.get('items',[]):
+                if wf==0:
+                    if search_result['id']['kind'] !='youtube#channel':
+                        continue
+                    print(json.dumps(search_result,indent=2,ensure_ascii=False),file=f)
+                    wf=1
+                else:
+                    break
 
 
+    def git(self,fn):
+        #自動commit
+        gad='git add ' + fn
+        os.system(gad)
+        
+
+
+    def main(self):
+        syt = Search_Youtube()
+        print('please input a  keyword.')
+        search_word=input()
+
+        #search dirのjsonファイルに結果を書きこむ
+
+        fn='search/search_' + search_word + '.json'
+        syt.make_file(fn,search_word)
+        syt.git(fn)
+
+
+if __name__ == '__main__':
+    syt = Search_Youtube()
+    syt.main()
