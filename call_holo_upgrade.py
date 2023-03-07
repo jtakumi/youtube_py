4eb16ae4a68936787ff4e90e20e751a87c270749
jindo takumi <92016292+jtakumi@users.noreply.github.com>
2023-03-07 15:41:53 +0900

    tiddy up

--- call_holo_upgrade.py
+++
@@ -1,9 +0,0 @@
-import os
-import upgrade_hololive_subscriber as uhs
-import upgrade_hololive_videocount as uhv
-
-
-def main():
-    uhs.main()
-    uhv.main()
-
--- looptest.py
+++
@@ -1,9 +0,0 @@
-import os
-from os import listdir
-
-dir='./hololive'
-
-i=1
-for i in range(1,len(listdir(dir))):
-    print(i)
-    
